f29599f8401104c4c977e13d90ef7f50356e8423
sun <sts07190@naver.com>
2026-08-11 15:41:49 +0900

    [setting] 프로젝트 생성

---
+++ Munecting/Munecting/MunectingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunectingApp.swift
+//  Munecting
+//
+//  Created by sun on 8/11/26.
+//
+
+import SwiftUI
+
+@main
+struct MunectingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
